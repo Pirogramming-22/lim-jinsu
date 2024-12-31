4affc1d005404a35f1aa38c9c8e9f448b11c5125
imjiinsoo <jsbd0109@naver.com>
2025-01-01 00:27:07 +0900

    JavaScript-display items

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -2,16 +2,32 @@
 // Fetch the items from the JSON file
 function loadItems() {
     return fetch('data/data.json')
-      .then(response => response,json())
+      .then(response => response.json())
       .then(json => json.items);
 }
 
+//update the list with the given items
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    const html = items.map(item => createHTMLString(item)).join('');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
 
+
+// creat HTML list item from the given data item
+function createHTMLString(item) {
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>    
+    `;
+}
 // main
 loadItems() 
   .then(items => {
     console.log(items);
- //   displayItems(items);
+    displayItems(items);
  //   setEventListeners(items)
   })
   .catch(console.log)
