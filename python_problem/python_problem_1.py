f514c97c9dbdb8279daae8de9bb6d56fb09aca09
imjiinsoo <jsbd0109@naver.com>
2025-01-03 01:59:17 +0900

    3단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,3 +1,22 @@
+# 1단계
 num = 0
 
-cnt = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+# 2단계 3단계 
+while True:
+    user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+
+    # 정수인지 검사
+    if not user_input.isdigit():
+        print("정수를 입력하세요")
+        continue
+
+    # 정수로 변환
+    cnt = int(user_input)
+    
+    # 1, 2, 3 중 하나인지 검사
+    if cnt not in [1, 2, 3]:
+        print("1, 2, 3 중 하나를 입력하세요")
+        continue
+
+    # 검사를 통과하면 루프 종료
+    break
