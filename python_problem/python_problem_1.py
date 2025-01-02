c4b02f23743e6f593268982465eb4858f8249037
imjiinsoo <jsbd0109@naver.com>
2025-01-03 02:27:45 +0900

    6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,52 +1,59 @@
-# 1단계
 num = 0
+end = False
+ 
+while not end:
+    while True:
+        user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
 
-# 2단계 3단계 
-while True:
-    user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+        # 정수인지 검사
+        if not user_input.isdigit():
+            print("정수를 입력하세요")
+            continue
 
-    # 정수인지 검사
-    if not user_input.isdigit():
-        print("정수를 입력하세요")
-        continue
+        # 정수로 변환
+        cnt = int(user_input)
+        
+        # 1, 2, 3 중 하나인지 검사
+        if cnt not in [1, 2, 3]:
+            print("1, 2, 3 중 하나를 입력하세요")
+            continue
 
-    # 정수로 변환
-    cnt = int(user_input)
-    
-    # 1, 2, 3 중 하나인지 검사
-    if cnt not in [1, 2, 3]:
-        print("1, 2, 3 중 하나를 입력하세요")
-        continue
+        # 검사를 통과하면 루프 종료
+        break
 
-    # 검사를 통과하면 루프 종료
-    break
+    for i in range(cnt):
+        num += 1
+        print(f"playerA : {num}")
+        if num == 31:
+            end = True
+            break
 
-# 4단계
-for i in range(cnt):
-    num += 1
-    print(f"playerA : {num}")
+    if end == True:
+        break
+ 
+    while True:
+        user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
 
-# 5단계 
-while True:
-    user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+        # 정수인지 검사
+        if not user_input.isdigit():
+            print("정수를 입력하세요")
+            continue
 
-    # 정수인지 검사
-    if not user_input.isdigit():
-        print("정수를 입력하세요")
-        continue
+        # 정수로 변환
+        cnt = int(user_input)
+        
+        # 1, 2, 3 중 하나인지 검사
+        if cnt not in [1, 2, 3]:
+            print("1, 2, 3 중 하나를 입력하세요")
+            continue
 
-    # 정수로 변환
-    cnt = int(user_input)
-    
-    # 1, 2, 3 중 하나인지 검사
-    if cnt not in [1, 2, 3]:
-        print("1, 2, 3 중 하나를 입력하세요")
-        continue
+        # 검사를 통과하면 루프 종료
+        break
 
-    # 검사를 통과하면 루프 종료
-    break
-
-# player B's turn
-for i in range(cnt):
-    num += 1
-    print(f"playerB : {num}")
+    # player B's turn
+    for i in range(cnt):
+        num += 1
+        print(f"playerB : {num}")
+        if num == 31:
+            end = True
+            break
