327c9042bab4dd9b58790cfc2699053c62f3757d
imjiinsoo <jsbd0109@naver.com>
2025-01-03 03:00:55 +0900

    9단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,26 +1,32 @@
+import random 
+
 num = 0
 end = False
 
 # brGame 함수 정의
 def brGame(player, num):
-    while True:
-        user_input = input(f"{player} 부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+    if player == "computer":
+        cnt = random.randint(1,3)
+
+    else:
+        while True:
+            user_input = input(f"{player} 부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
         
-        # 정수인지 검사
-        if not user_input.isdigit():
-            print("정수를 입력하세요")
-            continue
-        
-        # 정수로 변환
-        cnt = int(user_input)
-        
-        # 1, 2, 3 중 하나인지 검사
-        if cnt not in [1, 2, 3]:
-            print("1, 2, 3 중 하나를 입력하세요")
-            continue
-        
-        # 검사를 통과하면 루프 종료
-        break
+            # 정수인지 검사
+            if not user_input.isdigit():
+                print("정수를 입력하세요")
+                continue
+            
+            # 정수로 변환
+            cnt = int(user_input)
+            
+            # 1, 2, 3 중 하나인지 검사
+            if cnt not in [1, 2, 3]:
+                print("1, 2, 3 중 하나를 입력하세요")
+                continue
+            
+            # 검사를 통과하면 루프 종료
+            break
 
     for i in range(cnt):
         num += 1
@@ -32,14 +38,14 @@ def brGame(player, num):
 
 # 메인 게임 루프
 while not end:
-    # PlayerA의 차례
-    num, end = brGame("playerA", num)
+    # computer의 차례
+    num, end = brGame("computer", num)
     if end:
-        print("PlayerB win!")
+        print("player win!")
         break
 
-    # PlayerB의 차례
-    num, end = brGame("playerB", num)
+    # Player의 차례
+    num, end = brGame("player", num)
     if end:
-        print("PlayerA win!")
+        print("computer win!")
         break
