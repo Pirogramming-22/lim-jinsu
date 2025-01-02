e976a3f19b171108d7b8b8d514732b7df8ab09af
imjiinsoo <jsbd0109@naver.com>
2025-01-03 02:47:32 +0900

    8단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,61 +1,45 @@
 num = 0
 end = False
 
+# brGame 함수 정의
+def brGame(player, num):
+    while True:
+        user_input = input(f"{player} 부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+        
+        # 정수인지 검사
+        if not user_input.isdigit():
+            print("정수를 입력하세요")
+            continue
+        
+        # 정수로 변환
+        cnt = int(user_input)
+        
+        # 1, 2, 3 중 하나인지 검사
+        if cnt not in [1, 2, 3]:
+            print("1, 2, 3 중 하나를 입력하세요")
+            continue
+        
+        # 검사를 통과하면 루프 종료
+        break
+
+    for i in range(cnt):
+        num += 1
+        print(f"{player} : {num}")
+        if num == 31:
+            return num, True  # num과 게임 종료 상태 반환
+    
+    return num, False  # num과 게임 미종료 상태 반환
+
+# 메인 게임 루프
 while not end:
-    while True:
-        user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-
-        # 정수인지 검사
-        if not user_input.isdigit():
-            print("정수를 입력하세요")
-            continue
-
-        # 정수로 변환
-        cnt = int(user_input)
-        
-        # 1, 2, 3 중 하나인지 검사
-        if cnt not in [1, 2, 3]:
-            print("1, 2, 3 중 하나를 입력하세요")
-            continue
-
-        # 검사를 통과하면 루프 종료
+    # PlayerA의 차례
+    num, end = brGame("playerA", num)
+    if end:
+        print("PlayerB win!")
         break
 
-    for i in range(cnt):
-        num += 1
-        print(f"playerA : {num}")
-        if num == 31:
-            print("playerB win!")
-            end = True
-            break
-
-    if end == True:
+    # PlayerB의 차례
+    num, end = brGame("playerB", num)
+    if end:
+        print("PlayerA win!")
         break
-
-    while True:
-        user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-
-        # 정수인지 검사
-        if not user_input.isdigit():
-            print("정수를 입력하세요")
-            continue
-
-        # 정수로 변환
-        cnt = int(user_input)
-        
-        # 1, 2, 3 중 하나인지 검사
-        if cnt not in [1, 2, 3]:
-            print("1, 2, 3 중 하나를 입력하세요")
-            continue
-
-        # 검사를 통과하면 루프 종료
-        break
-
-    # player B's turn
-    for i in range(cnt):
-        num += 1
-        print(f"playerB : {num}")
-        if num == 31:
-            print("playerA win!")
-            end = True
-            break
